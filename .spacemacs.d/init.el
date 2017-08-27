fa0a98be466fa796d92250d2e521cc8c72c321f6
Jeff Owens <dmin7b5@gmail.com>
2017-08-27 08:54:39 -0500

    Latest init.el changes.  Added org-journal-mode

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -18,6 +18,7 @@ values."
    ;; of a list then all discovered layers will be installed.
    dotspacemacs-configuration-layers
    '(
+     php
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
@@ -81,11 +82,7 @@ values."
             ("irc.gitter.im"
              :port "6697"
              :ssl t
-             :nick "dmin7b5")
-            ("semaphoremobile.irc.slack.com"
-             :port "6697"
-             :ssl t
-             :nick "jeff")))
+             :nick "dmin7b5")))
      ;; (shell :variables
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
@@ -97,6 +94,7 @@ values."
    ;; packages then consider to create a layer, you can also put the
    ;; configuration in `dotspacemacs/config'.
    dotspacemacs-additional-packages '(
+                                      org-gcal
                                       org-journal
                                       company-emoji
                                       sos
@@ -104,6 +102,7 @@ values."
                                       helm-org-rifle
                                       hackernews
                                       yatemplate
+                                      (magithub :location (recipe :fetcher github :repo "vermiculus/magithub" :branch "develop"))
                                       )
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()
@@ -273,6 +272,13 @@ layers configuration. You are free to put any user code."
    ;; Set the shell type
    ;;(setq explicit-shell-file-name "/bin/zsh")
 
+   ;; Set Org Mode Super/Subscript to off
+   (setq org-use-sub-superscripts nil)
+
+   ;; Magithub
+   (require 'magithub)
+   (magithub-feature-autoinject t)
+
    ;; YASnippet Dir
    (setq yas-snippet-dirs
          '("~/.spacemacs.d/snippets"))
@@ -286,15 +292,51 @@ layers configuration. You are free to put any user code."
    (setq org-pretty-entities t)
    (setq org-log-into-drawer t)
 
+   ;; Org calendar
+   (require 'org-gcal)
+   (setq org-gcal-client-id "446726852703-00edme23in17epr5gp8dpofkpetuq284.apps.googleusercontent.com"
+         org-gcal-client-secret "e2ck3B8tG3mh7owfmZFkZote"
+         org-gcal-file-alist '(("dmin7b5@gmail.com" .  "~/CloudStation/Files/Jeff/Org/gcal.org")))
+   ;; org-gcal hooks
+   (add-hook 'org-agenda-mode-hook (lambda () (org-gcal-sync) ))
+
    ;; Org agenda
    (setq org-agenda-files (list "~/CloudStation/Files/Jeff/Org/work.org"
                                 "~/CloudStation/Files/Jeff/Org/index.org"
+                                "~/CloudStation/Files/Jeff/Org/gcal.org"
                                 "~/CloudStation/Files/Jeff/Org/mylife.org"
                                 "~/CloudStation/Files/Jeff/Org/home.org"))
+
    ;; mobileorg settings
    (setq org-mobile-inbox-for-pull "~/CloudStation/Files/Jeff/Org/index.org")
    (setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
    (setq org-mobile-files '("~/CloudStation/Files/Jeff/Org"))
+   (setq org-mobile-use-encryption t)
+   (setq org-mobile-encryption-password "W2z970140")
+
+   (defvar my-org-mobile-sync-timer nil)
+
+   (defvar my-org-mobile-sync-secs (* 60 20))
+
+   (defun my-org-mobile-sync-pull-and-push ()
+     (org-mobile-pull)
+     (org-mobile-push)
+     (when (fboundp 'sauron-add-event)
+       (sauron-add-event 'my 3 "Called org-mobile-pull and org-mobile-push")))
+
+   (defun my-org-mobile-sync-start ()
+     "Start automated `org-mobile-push'"
+     (interactive)
+     (setq my-org-mobile-sync-timer
+           (run-with-idle-timer my-org-mobile-sync-secs t
+                                'my-org-mobile-sync-pull-and-push)))
+
+   (defun my-org-mobile-sync-stop ()
+     "Stop automated `org-mobile-push'"
+     (interactive)
+     (cancel-timer my-org-mobile-sync-timer))
+
+   (my-org-mobile-sync-start)
 
    ;; org-journal
    (setq org-journal-dir "~/CloudStation/Files/Jeff/Org/Journal"
@@ -315,6 +357,9 @@ layers configuration. You are free to put any user code."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("/Users/wmac/CloudStation/Files/Jeff/Org/agenda.org" "/Users/wmac/CloudStation/Files/Jeff/Org/chess.org" "/Users/wmac/CloudStation/Files/Jeff/Org/home.org" "/Users/wmac/CloudStation/Files/Jeff/Org/inbox.org" "/Users/wmac/CloudStation/Files/Jeff/Org/index.org" "/Users/wmac/CloudStation/Files/Jeff/Org/mylife.org" "/Users/wmac/CloudStation/Files/Jeff/Org/testnote.org" "/Users/wmac/CloudStation/Files/Jeff/Org/todo.org" "/Users/wmac/CloudStation/Files/Jeff/Org/work.org")))
  '(org-capture-templates
    (quote
     (("p" "Private Templates")
@@ -324,7 +369,8 @@ layers configuration. You are free to put any user code."
       :empty-lines-before 1))))
  '(package-selected-packages
    (quote
-    (flycheck helm helm-core magit js2-mode yasnippet yatemplate zonokai-theme zenburn-theme zen-and-art-theme yapfify yaml-mode xterm-color ws-butler window-numbering which-key web-mode web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package underwater-theme ujelly-theme typit twilight-theme twilight-bright-theme twilight-anti-bright-theme tronesque-theme toxi-theme toc-org tao-theme tangotango-theme tango-plus-theme tango-2-theme tagedit sunny-day-theme sublime-themes subatomic256-theme subatomic-theme stickyfunc-enhance srefactor spacemacs-theme spaceline spacegray-theme sos soothe-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme smeargle slim-mode shell-pop seti-theme scss-mode sass-mode rvm ruby-tools ruby-test-mode rubocop rspec-mode robe reverse-theme reveal-in-osx-finder restart-emacs rbenv rake rainbow-mode rainbow-identifiers rainbow-delimiters railscasts-theme quelpa pyvenv pytest pyenv-mode py-isort purple-haze-theme pug-mode professional-theme popwin planet-theme pip-requirements phoenix-dark-pink-theme phoenix-dark-mono-theme persp-mode pbcopy pastels-on-dark-theme paradox pacmacs osx-trash osx-dictionary orgit organic-green-theme org-projectile org-present org-pomodoro org-plus-contrib org-journal org-download org-bullets open-junk-file omtose-phellack-theme oldlace-theme occidental-theme obsidian-theme ob-elixir noctilux-theme niflheim-theme neotree naquadah-theme mwim mustang-theme multi-term move-text monokai-theme monochrome-theme molokai-theme moe-theme mmm-mode minitest minimal-theme material-theme markdown-toc majapahit-theme magit-gitflow magit-gh-pulls macrostep lush-theme lorem-ipsum livid-mode live-py-mode linum-relative link-hint light-soap-theme less-css-mode launchctl json-mode js2-refactor js-doc jinja2-mode jbeans-theme jazz-theme ir-black-theme insert-shebang inkpot-theme info+ indent-guide impatient-mode ido-vertical-mode hy-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt heroku-theme hemisu-theme help-fns+ helm-themes helm-swoop helm-pydoc helm-projectile helm-org-rifle helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-dash helm-css-scss helm-company helm-c-yasnippet helm-ag hc-zenburn-theme hackernews gruvbox-theme gruber-darker-theme graphviz-dot-mode grandshell-theme gotham-theme google-translate golden-ratio gnuplot github-search github-clone github-browse-file gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gist gh-md gandalf-theme flycheck-pos-tip flycheck-mix flx-ido flatui-theme flatland-theme fish-mode firebelly-theme fill-column-indicator farmhouse-theme fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-commentary evil-args evil-anzu eval-sexp-fu espresso-theme eshell-z eshell-prompt-extras esh-help erlang erc-yt erc-view-log erc-terminal-notifier erc-social-graph erc-image erc-hl-nicks emoji-cheat-sheet-plus emmet-mode elisp-slime-nav dumb-jump dracula-theme django-theme disaster deft dash-at-point darktooth-theme darkokai-theme darkmine-theme darkburn-theme dakrone-theme cython-mode cyberpunk-theme company-web company-tern company-statistics company-shell company-emoji company-c-headers company-anaconda column-enforce-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-identifiers-mode coffee-mode cmake-mode clues-theme clean-aindent-mode clang-format chruby cherry-blossom-theme busybee-theme bundler bubbleberry-theme birds-of-paradise-plus-theme badwolf-theme auto-yasnippet auto-highlight-symbol auto-compile apropospriate-theme anti-zenburn-theme ansible-doc ansible ample-zen-theme ample-theme alect-themes alchemist aggressive-indent afternoon-theme adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell 2048-game)))
+    (phpunit phpcbf php-extras php-auto-yasnippets drupal-mode php-mode powerline org-category-capture undo-tree skewer-mode magithub ghub+ apiwrap ghub mmt org diminish packed with-editor gh markdown-mode log4e hydra f s anaconda-mode company smartparens highlight evil multiple-cursors avy alert projectile magit-popup git-commit async inf-ruby haml-mode dash org-gcal request-deferred deferred flycheck helm helm-core magit js2-mode yasnippet yatemplate zonokai-theme zenburn-theme zen-and-art-theme yapfify yaml-mode xterm-color ws-butler window-numbering which-key web-mode web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package underwater-theme ujelly-theme typit twilight-theme twilight-bright-theme twilight-anti-bright-theme tronesque-theme toxi-theme toc-org tao-theme tangotango-theme tango-plus-theme tango-2-theme tagedit sunny-day-theme sublime-themes subatomic256-theme subatomic-theme stickyfunc-enhance srefactor spacemacs-theme spaceline spacegray-theme sos soothe-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme smeargle slim-mode shell-pop seti-theme scss-mode sass-mode rvm ruby-tools ruby-test-mode rubocop rspec-mode robe reverse-theme reveal-in-osx-finder restart-emacs rbenv rake rainbow-mode rainbow-identifiers rainbow-delimiters railscasts-theme quelpa pyvenv pytest pyenv-mode py-isort purple-haze-theme pug-mode professional-theme popwin planet-theme pip-requirements phoenix-dark-pink-theme phoenix-dark-mono-theme persp-mode pbcopy pastels-on-dark-theme paradox pacmacs osx-trash osx-dictionary orgit organic-green-theme org-projectile org-present org-pomodoro org-plus-contrib org-journal org-download org-bullets open-junk-file omtose-phellack-theme oldlace-theme occidental-theme obsidian-theme ob-elixir noctilux-theme niflheim-theme neotree naquadah-theme mwim mustang-theme multi-term move-text monokai-theme monochrome-theme molokai-theme moe-theme mmm-mode minitest minimal-theme material-theme markdown-toc majapahit-theme magit-gitflow magit-gh-pulls macrostep lush-theme lorem-ipsum livid-mode live-py-mode linum-relative link-hint light-soap-theme less-css-mode launchctl json-mode js2-refactor js-doc jinja2-mode jbeans-theme jazz-theme ir-black-theme insert-shebang inkpot-theme info+ indent-guide impatient-mode ido-vertical-mode hy-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt heroku-theme hemisu-theme help-fns+ helm-themes helm-swoop helm-pydoc helm-projectile helm-org-rifle helm-mode-manager helm-make helm-gitignore helm-flx helm-descbinds helm-dash helm-css-scss helm-company helm-c-yasnippet helm-ag hc-zenburn-theme hackernews gruvbox-theme gruber-darker-theme graphviz-dot-mode grandshell-theme gotham-theme google-translate golden-ratio gnuplot github-search github-clone github-browse-file gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gist gh-md gandalf-theme flycheck-pos-tip flycheck-mix flx-ido flatui-theme flatland-theme fish-mode firebelly-theme fill-column-indicator farmhouse-theme fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-mc evil-matchit evil-magit evil-lisp-state evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-commentary evil-args evil-anzu eval-sexp-fu espresso-theme eshell-z eshell-prompt-extras esh-help erlang erc-yt erc-view-log erc-terminal-notifier erc-social-graph erc-image erc-hl-nicks emoji-cheat-sheet-plus emmet-mode elisp-slime-nav dumb-jump dracula-theme django-theme disaster deft dash-at-point darktooth-theme darkokai-theme darkmine-theme darkburn-theme dakrone-theme cython-mode cyberpunk-theme company-web company-tern company-statistics company-shell company-emoji company-c-headers company-anaconda column-enforce-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-identifiers-mode coffee-mode cmake-mode clues-theme clean-aindent-mode clang-format chruby cherry-blossom-theme busybee-theme bundler bubbleberry-theme birds-of-paradise-plus-theme badwolf-theme auto-yasnippet auto-highlight-symbol auto-compile apropospriate-theme anti-zenburn-theme ansible-doc ansible ample-zen-theme ample-theme alect-themes alchemist aggressive-indent afternoon-theme adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell 2048-game)))
+ '(reb-re-syntax (quote read))
  '(send-mail-function (quote mailclient-send-it)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
